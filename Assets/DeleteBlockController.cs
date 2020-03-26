a7ebe9caf6385002d8dbf3a08c6242d5922bdd36
swk-0111 <sawako.mohri@i.softbank.jp>
2020-03-26 22:26:47 +0900

    Lesson6課題提出
    Lesson6の課題を提出いたします。発展課題は含まれていません。

---
+++ Assets/DeleteBlockController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteBlockController : MonoBehaviour {
+
+    //透明な壁を移動させるコンポーネントを入れる
+   // private Rigidbody myRigidbody;
+    //前進するための力
+   // private float forwardForce = 800.0f;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Rigidbodyコンポーネントを取得
+        // this.myRigidbody = GetComponent<Rigidbody>();
+
+        //Debug.Log("スタート");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //透明な壁に前方向の力を加える
+       // this.myRigidbody.AddForce(this.transform.forward * forwardForce);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("衝突した");
+        //Debug.Log("アザー" + other.gameObject.tag);
+
+
+        //障害物に衝突した場合
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+           // Debug.Log("デリート前");
+            Destroy(other.gameObject);
+           // Debug.Log("デリート後");
+        }
+
+        // Debug.Log("if文抜けた");
+
+    }
+}
---
+++ Assets/MyCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour {
+
+    //Unityちゃんオブジェクト
+    private GameObject unitychan;
+    //Unityちゃんとカメラの距離
+    private float difference;
+
+    //test 障害物を消すブロックのオブジェクト
+    private GameObject deleteBlock;
+
+    //test 障害物を消すブロックとカメラの位置
+    private float blockDifference;
+
+
+    // Use this for initialization
+    void Start () {
+        //Unityちゃんのオブジェクトを取得
+        this.unitychan = GameObject.Find ("unitychan");
+        //Unityちゃんとカメラの位置(z座標)の差を求める
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+        //test 障害物を消すブロックのオブジェクトを取得
+        this.deleteBlock = GameObject.Find("DeleteBlock");
+        //障害物を消すブロックの位置はカメラと同じ
+        this.blockDifference = this.transform.position.z;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //Unityちゃんの位置に合わせてカメラの位置を移動
+        this.transform.position = new Vector3(0, 
+                                              this.transform.position.y,
+                                              this.unitychan.transform.position.z - difference);
+
+        //Debug.Log(this.transform.position);
+    }
+}
